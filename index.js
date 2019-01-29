b9ab5d49143f9ab2b94cfe94838f13062613b08a
mmbensalah <mmbensalah@gmail.com>
2019-01-29 15:41:58 -0700

    Create post request for api/v1/foods

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const pry  = require('pryjs')
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
@@ -18,6 +19,26 @@ app.get('/api/v1/foods', (request, response) => {
     })
     .catch((error) => {
       response.status(500).json({ error });
+    });
+});
+
+app.post('/api/v1/foods', (request, response) => {
+  const food = request.body;
+
+  for (let requiredParameter of ['name', 'calories']) {
+    if (!food[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { title: <String>, author: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('foods').insert(food, 'id')
+    .then(food => {
+      response.status(201).json({ "food": request.body })
+    })
+    .catch(error => {
+      response.status(400).json({ error });
     });
 });
 
