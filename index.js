a8ba1c2377e61e57f26df40f11d89ec67e8252a2
mmbensalah <mmbensalah@gmail.com>
2019-01-31 11:54:55 -0700

    Modify GET api/v1/foods/:id to return an object, not an array with a nested object. Modify test accordingly

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/api/v1/foods/:id', (request, response) => {
   database('foods').where('id', request.params.id).select()
     .then(foods => {
       if (foods.length) {
-        response.status(200).json(foods);
+        response.status(200).json(foods[0]);
       } else {
         response.status(404).json({
           error: `Could not find food with id ${request.params.id}`
